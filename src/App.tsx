9211782f73b0801a7e559b972451fe313138ed06
Shota Narisaka <shootacean@gmail.com>
2024-04-07 22:11:38 +0000

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import { useState } from 'react'
-import { Grid, GridItem, Button, Center } from '@chakra-ui/react'
+import { Button, Center, Grid, GridItem } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Grid
-        templateAreas={`"header header"
+	return (
+		<>
+			<Grid
+				templateAreas={`"header header"
                         "nav main"
                         "footer footer"`}
-        gridTemplateRows={'50px auto 50px'}
-        gridTemplateColumns={'150px 1fr'}
-        h='100vh'
-        gap='1'
-        color='blackAlpha.700'
-        fontWeight='bold'
-      >
-        <GridItem pl='2' bg='orange.300' area={'header'}>
-          Header
-        </GridItem>
-        <GridItem pl='2' bg='pink.300' area={'nav'}>
-          Nav
-        </GridItem>
-        <GridItem pl='2' bg='green.300' area={'main'}>
-          <Center height='full'>
-            <Button colorScheme='blue' onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </Button>
-          </Center>
-        </GridItem>
-        <GridItem pl='2' bg='blue.300' area={'footer'}>
-          Footer
-        </GridItem>
-      </Grid>
-    </>
-  )
+				gridTemplateRows={"50px auto 50px"}
+				gridTemplateColumns={"150px 1fr"}
+				h="100vh"
+				gap="1"
+				color="blackAlpha.700"
+				fontWeight="bold"
+			>
+				<GridItem pl="2" bg="orange.300" area={"header"}>
+					Header
+				</GridItem>
+				<GridItem pl="2" bg="pink.300" area={"nav"}>
+					Nav
+				</GridItem>
+				<GridItem pl="2" bg="green.300" area={"main"}>
+					<Center height="full">
+						<Button
+							colorScheme="blue"
+							onClick={() => setCount((count) => count + 1)}
+						>
+							count is {count}
+						</Button>
+					</Center>
+				</GridItem>
+				<GridItem pl="2" bg="blue.300" area={"footer"}>
+					Footer
+				</GridItem>
+			</Grid>
+		</>
+	);
 }
 
-export default App
+export default App;
